757f60ab4c9934802759a41f98db96741e3eed1c
Will Abson <will.abson@gmail.com>
2017-02-14 16:06:18 +0000

    Working script for importing demo data
    Refs #12

--- README.md
+++ README.md
@@ -1,17 +1,45 @@
-# ALFRESCO HEALTH CARE DEMO APP
+# Alfresco Healthcare Demo Application
 
 <p align="center">
   <img title="alfresco" alt='alfresco' src='assets/alfresco.png'  width="280px" height="150px"></img>
   <img title="angular2" alt='angular2' src='assets/angular2.png'  width="150px" height="150px"></img>
 </p>
 
+This project provides an example vertical-focussed app utilising the
+[Alfresco Angular2 components](http://github.com/Alfresco/alfresco-ng2-components). The app includes a
+customised theme, customised instances of the out-of-the-box content and process components, some custom
+local components and a set of content and process demo data.
+
 ## Prerequisites
 
-Before you start using this development framework, make sure you have installed all required software and done all the
+Before you start using the app, make sure you have installed all required software and done all the
 necessary configuration, see this [page](PREREQUISITES.md).
+
+## Quick start
+
+If you have fulfilled the prerequisites then you can install dependencies and start up the app
+
+    npm install
+    npm run start
 
 ##Configure ECM and BPM
 
-- Config BPM: [page](assets/bpm/README.md).
+The project contains a script `bootstrap.sh` that you can use to import all the necessary data into your
+environment automatically.
 
-- Config ECM: [page](assets/ecm/README.md).
+The script assumes you have Alfresco Content Services running on port 8080 and Alfresco Process Services
+on port 9999, on the same host, which you must specify if this is not your local machine
+
+    ./scripts/bootstrap.sh          # will assume http://localhost
+
+Or for a remote environment
+
+    ./scripts/bootstrap.sh https://my.remote.env
+
+*Warning: this script has only been tested on clean environments without any existing data present. Use
+this at your own risk in a system with other data already present.*
+
+###Manual set-up
+
+If you prefer you can set up the application data for [content services data](assets/ecm/README.md) and
+[process services data](assets/bpm/README.md) manually.
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,68 +1,161 @@
 #!/bin/bash
 
-ecm_url='http://localhost:8080/alfresco'
-ecm_user='admin'
-ecm_pass='admin'
+base_url="$1"
+test -z "$base_url" && base_url="http://localhost"
 
-bpm_url='http://localhost:9999/activiti-app'
-bpm_user='admin@app.activiti.com'
-bpm_pass='admin'
+ecm_url="$base_url:8080/alfresco"
+ecm_user="admin"
+ecm_pass="admin"
+
+bpm_url="$base_url:9999/activiti-app"
+bpm_user="admin@app.activiti.com"
+bpm_pass="admin"
+
+agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:51.0) Gecko/20100101 Firefox/51.0'
+csrf_token="5762ca58a7a536f470fe21f79a2eb8b29b"
+
+function extract_id_from_json() {
+  [[ "$1" =~ ^\{\"id\":\"?([^\",]+)\"? ]] && echo "${BASH_REMATCH[1]}" || echo ""
+}
 
 function bpm_create_user() {
-  curl \
-    -u "$ecm_user:$bpm_pass" \
-    -d "{\"firstName\": \"$1\", \"lastName\": \"$2\", \"email\": \"$3\", \"password\": \"$4\", \"status\": \"active\", \"type\": \"enterprise\"}" \
-    "$bpm_url/api/enterprise/admin/users"
+  tenant_id="$5"
+  test -z "$5" && tenant_id=1
+  local resp=$( curl -s \
+    -u "$bpm_user:$bpm_pass" \
+    -A "$agent" \
+    -H "Cookie: CSRF-TOKEN=$csrf_token" \
+    -H "X-CSRF-TOKEN: $csrf_token" \
+    -H 'Content-Type: application/json' \
+    -d "{ \"firstName\": \"$1\", \"lastName\": \"$2\", \"email\": \"$3\", \"password\": \"$4\", \"status\": \"active\", \"type\": \"enterprise\", \"tenantId\": \"$tenant_id\" }" \
+    "$bpm_url/api/enterprise/admin/users" )
+  echo $( extract_id_from_json "$resp" )
 }
 
 function bpm_import_app() {
-  curl \
-    -u "$ecm_user:$bpm_pass" \
-    -F file=$1 \
-    "$bpm_url/api/enterprise/app-definitions/import"
+  local resp=$( curl \
+    -u "$bpm_user:$bpm_pass" \
+    -A "$agent" \
+    -H "Cookie: CSRF-TOKEN=$csrf_token" \
+    -H "X-CSRF-TOKEN: $csrf_token" \
+    -F file=@$1 \
+    "$bpm_url/api/enterprise/app-definitions/import?renewIdmEntries=true" )
+  echo $( extract_id_from_json "$resp" )
 }
 
 function bpm_publish_app() {
   curl \
-    -u "$ecm_user:$bpm_pass" \
-    -F file=$1 \
-    "$bpm_url/api/enterprise/app-definitions/import"
+    -u "$bpm_user:$bpm_pass" \
+    -A "$agent" \
+    -H "Cookie: CSRF-TOKEN=$csrf_token" \
+    -H "X-CSRF-TOKEN: $csrf_token" \
+    -H 'Content-Type: application/json' \
+    -d "{ \"comment\": \"\", \"force\": false }" \
+    "$bpm_url/api/enterprise/app-definitions/$1/publish"
+}
+
+function bpm_deploy_app() {
+  curl \
+    -u "$bpm_user:$bpm_pass" \
+    -A "$agent" \
+    -H "Cookie: CSRF-TOKEN=$csrf_token" \
+    -H "X-CSRF-TOKEN: $csrf_token" \
+    -H 'Content-Type: application/json' \
+    -d "{ \"appDefinitions\": [{\"id\":$1}] }" \
+    "$bpm_url/api/enterprise/runtime-app-definitions"
 }
 
 function bpm_create_model() {
-  curl \
-    -u "$ecm_user:$bpm_pass" \
+  local resp=$( curl \
+    -u "$bpm_user:$bpm_pass" \
+    -A "$agent" \
+    -H "Cookie: CSRF-TOKEN=$csrf_token" \
+    -H "X-CSRF-TOKEN: $csrf_token" \
+    -H 'Content-Type: application/json' \
     -d "{\"modelType\": $1, \"name\": \"$2\", \"description\": \"$3\" }" \
-    "$bpm_url/api/enterprise/models"
+    "$bpm_url/api/enterprise/models" )
+  echo "$resp" >&2
+  echo $( extract_id_from_json "$resp" )
+}
+
+function bpm_save_form() {
+  curl \
+    -X 'PUT' \
+    -u "$bpm_user:$bpm_pass" \
+    -A "$agent" \
+    -H "Cookie: CSRF-TOKEN=$csrf_token" \
+    -H "X-CSRF-TOKEN: $csrf_token" \
+    -H 'Content-Type: application/json' \
+    --data-binary "@$2" \
+    "$bpm_url/api/enterprise/editor/form-models/$1"
+
 }
 
 function ecm_create_user() {
   curl \
     -u "$ecm_user:$ecm_pass" \
-    -d "{\"personBodyCreate\": { \"id\": \"$4\", \"firstName\": \"$1\", \"lastName\": \"$2\", \"email\": \"$3\", \"enabled\": true, \"password\": \"$5\" } }" \ \
+    -H 'Content-Type: application/json' \
+    -d "{ \"id\": \"$4\", \"firstName\": \"$1\", \"lastName\": \"$2\", \"email\": \"$3\", \"enabled\": true, \"password\": \"$5\" }" \
     "$ecm_url/api/-default-/public/alfresco/versions/1/people"
 }
 
-function ecm_upload_model() {
+function ecm_create_site() {
   curl \
     -u "$ecm_user:$ecm_pass" \
-    -F file=$1 \
+    -H 'Content-Type: application/json' \
+    -d "{ \"id\": \"$1\", \"title\": \"$2\", \"description\": \"$3\", \"visibility\": \"$4\" }" \
+    "$ecm_url/api/-default-/public/alfresco/versions/1/sites"
+}
+
+function ecm_add_site_member() {
+  curl \
+    -u "$ecm_user:$ecm_pass" \
+    -H 'Content-Type: application/json' \
+    -d "{ \"id\": \"$2\", \"role\": \"$3\" }" \
+    "$ecm_url/api/-default-/public/alfresco/versions/1/sites/$1/members"
+}
+
+function ecm_upload_model() {
+  curl -s \
+    -u "$ecm_user:$ecm_pass" \
+    -F file=@$1 \
     "$ecm_url/s/api/cmm/upload"
 }
 
-#TODO check if user present first
-bpm_create_user "Visitor" "Visitor" "visitor@visitor.com" "visitor"
-#TODO check if app present first and update if found
-#TODO need to renew the user and group identifiers like option in Kickstart?
-bpm_import_app "../assets/bpm/app/visit.zip"
-#TODO deal with app not having ID 1
-bpm_publish_app 1
-#bpm_create_model 2 "Health_User_Form" "Patient details form"
-#TODO add actual form content from assets/bpm/forms/Health_User_Form.json
+function ecm_update_model_status() {
+  model_json=$( curl -s \
+    -u "$ecm_user:$ecm_pass" \
+    -H 'Content-Type: application/json' \
+    "$ecm_url/api/-default-/private/alfresco/versions/1/cmm/$1" )
+  echo "$model_json"
+  model_json=$( echo "$model_json" | sed -e "s/\"status\":\"[A-Z]*\"/\"status\":\"$2\"/" -e 's/^{"entry"://' -e 's/}}/}/' )
+  echo "$model_json"
+  curl \
+    -u "$ecm_user:$ecm_pass" \
+    -X PUT \
+    -H 'Content-Type: application/json' \
+    -d "$model_json" \
+    "$ecm_url/api/-default-/private/alfresco/versions/1/cmm/$1?select=status"
+}
+
+bpm_create_user "Visitor" "Visitor" "visitor@visitor.com" "visitor" >/dev/null
+
+app_id=$( bpm_import_app "../assets/bpm/app/visit.zip" )
+test -z "$app_id" && echo "Could not find app ID" && exit 1
+bpm_publish_app "$app_id"
+bpm_deploy_app "$app_id"
+
+form_id=$( bpm_create_model 2 "Health_User_Form" "Patient details form" )
+bpm_save_form "$form_id" "../assets/bpm/forms/Health_User_Form.import.json"
 
 ecm_create_user "Activiti" "Admin" "$bpm_user" "$bpm_user" "$bpm_pass"
 ecm_create_user "Visitor" "Visitor" "visitor@visitor.com" "visitor@visitor.com" "visitor"
 
-#TODO ECM models need activating
 ecm_upload_model "../assets/ecm/healthCareModel.zip"
+ecm_update_model_status "healthCareModel" "ACTIVE"
 ecm_upload_model "../assets/ecm/VisitData.zip"
+ecm_update_model_status "VisitData" "ACTIVE"
+
+# create site and add visitor user as collaborator
+ecm_create_site "health-visits" "Healthcare Visits" "Healthcare app patient data" "PUBLIC"
+ecm_add_site_member "health-visits" "$bpm_user" "SiteCollaborator"
